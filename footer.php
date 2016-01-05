2291419dbfade0b3bcad7f2e9c937f903396bcd0
s10n <a@akaiv.com>
2016-01-05 21:07:31 +0700

    index.html을 헤더, 메인, 푸터로 파일 나눔

---
+++ footer.php
@@ -0,0 +1,9 @@
+  </main><!-- .site-main -->
+
+  <footer class="site-footer">
+  </footer><!-- .site-footer -->
+
+  <script type="text/javascript" src="assets/jquery/dist/jquery.js?ver=1.11.3"></script>
+  <script type="text/javascript" src="js/script.js"></script>
+</body>
+</html>
--- header.php
+++ header.php
@@ -13,12 +13,3 @@
   </header><!-- .site-header -->
 
   <main class="site-main" role="main">
-  </main><!-- .site-main -->
-
-  <footer class="site-footer">
-  </footer><!-- .site-footer -->
-
-  <script type="text/javascript" src="assets/jquery/dist/jquery.js?ver=1.11.3"></script>
-  <script type="text/javascript" src="js/script.js"></script>
-</body>
-</html>
